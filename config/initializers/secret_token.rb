416681ea1d263addf1197090984a19884759912f
Scott Clark <clarkbarz@me.com>
2013-06-26 21:09:40 -0600

    secret key base and logfile added to gitignore

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 JacksonCo::Application.config.secret_token = '9ce0888d417f4cf7e9185e4620a05980ea2b572405423042afe344fadd108c9bda682d8a6d6745f72b1fe42e2f6ef9856f17f458336c4b6345bce18ab754f050'
+
+JacksonCo::Application.config.secret_key_base = '5bce9ce0888d412afe344faf17f8346423040a05980ea2b48c9bda682d8a6d6745f72b18ab736c4b6342e2f62dd10f0501fe572405ef98567f4cf457e9185e54'
