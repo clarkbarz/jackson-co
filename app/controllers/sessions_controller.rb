7b355c47bc96ad3a9d25ae624ef919a6828e2b37
Scott Clark <clarkbarz@me.com>
2013-06-26 22:49:38 -0600

    completed sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,18 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		customer = Customer.find_by(:email, params[:session][:email].downcase)
+		if customer && customer.authenticate(params[:session][:password])
+			sign_in customer
+			redirect_back_or customer
+		else
+			flash.now[:error] = 'Invalid email or password'
+			render 'new'
+		end
 	end
 
 	def destroy
+		sign_out
+		redirect_to root_url
 	end
 end
