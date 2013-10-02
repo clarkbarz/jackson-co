29db052eaa9650927f8702648e4885dce5970c5e
Scott Clark <clarkbarz@me.com>
2013-10-02 10:55:25 -0600

    updated show page, product#new page for admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,4 +12,21 @@ class ProductsController < ApplicationController
 		@products = Product.where("category = 'shirt'").paginate(page: params[:page])
 	end
 
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		alt_name = @product.name.downcase.split
+		@product.alt_name = alt_name.join("_")
+		redirect_to products_path if @product.save
+	end
+
+	private
+
+		def product_params
+			params.require(:product).permit(:name, :category, :description, :price)
+		end
+
 end
